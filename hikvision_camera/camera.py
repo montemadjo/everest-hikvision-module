d3e24fa1ab9ccc19e90e4d56c0b77be2fc860df9
Bogdan Maksimovic <bogdan.maksimovic@outlook.com>
2021-12-12 08:30:48 +0100

    ...

--- hikvision_camera/camera.py
+++ hikvision_camera/camera.py
@@ -1,5 +1,7 @@
 import requests
+from requests import auth
 from requests.auth import HTTPDigestAuth
+from requests.models import Response
 
 class Camera:
     def __init__(self, URL, username, password) -> None:
@@ -8,6 +10,24 @@ class Camera:
         self.password = password
         self.raw_high_request = '<IOPortData version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema"><outputState>high</outputState></IOPortData>'
         self.raw_low_request = '<IOPortData version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema"><outputState>low</outputState></IOPortData>'
+        self.raw_http_hosts_request = """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <HttpHostNotificationList version="2.0" xmlns="http://www.hikvision.com/ver20/XMLSchema">
+            <HttpHostNotification version="2.0" xmlns="http://www.hikvision.com/ver20/XMLSchema">
+                <id>1</id>
+                <url>/</url>
+                <protocolType>HTTP</protocolType>
+                <parameterFormatType>XML</parameterFormatType>
+                <addressingFormatType>ipaddress</addressingFormatType>
+                <ipAddress>192.168.1.120</ipAddress>
+                <portNo>8080</portNo>
+                <userName></userName>
+                <httpAuthenticationMethod>none</httpAuthenticationMethod>
+                <ANPR>
+                    <detectionUpLoadPicturesType opt="all,licensePlatePicture,detectionPicture">detectionPicture</detectionUpLoadPicturesType>
+                </ANPR>
+            </HttpHostNotification>
+        </HttpHostNotificationList>"""
 
     def postOutputRequest(self, state) -> None:
         if state is 1:
@@ -17,3 +37,9 @@ class Camera:
             response = requests.put(self.url, data=self.raw_low_request, auth=HTTPDigestAuth(self.username, self.password))
             print(response)
 
+    def subscribeToAnpr(self, ip_address, port):
+        print(ip_address)
+        print(port)
+        response = requests.put(self.url, data=self.raw_http_hosts_request,
+                                auth=HTTPDigestAuth(self.username, self.password))
+        return response
