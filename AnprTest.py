4ccdd3c94b913db2b1e236c00e063655047b66e9
Pavle Vojinovic <pavle.vojinovic@guardianworld.me>
2021-12-18 12:53:38 +0100

    ...

--- AnprTest.py
+++ AnprTest.py
@@ -3,10 +3,14 @@ from hikvision_camera import Camera
 if __name__ == '__main__':
     camera = Camera('192.168.1.152', 'admin', 'Tfs123456')
 
-    r = camera.getHttpHostsCapabilities()
+    # r = camera.getHttpHostsCapabilities()
+
+    # print(r.content.decode("utf-8"))
+
+    r = camera.getAnprPlates()
 
     print(r.content.decode("utf-8"))
 
-    r = camera.getAnprPlates()
+    r = camera.getAnprPicture()
 
     print(r.content.decode("utf-8"))
--- hikvision_camera/camera.py
+++ hikvision_camera/camera.py
@@ -7,10 +7,14 @@ class Camera:
     def __init__(self, URL, username, password) -> None:
         self.url_trigger = 'http://' + URL + '/ISAPI/System/IO/outputs/1/trigger'
         self.url_anpr_register = 'http://' + URL + ''
+        self.xxx = 'http://admin:Tfs123456@192.168.1.152/ISAPI/Traffic/channels/1/vehicleDetect/plates'
         self.url_anpr_get_cap = 'http://' + URL + '/ISAPI/Event/notification/httpHosts/capabilities'
         self.url_anpr_get_plates = 'http://' + URL + '/ISAPI/Traffic/channels/1/vehicleDetect/plates'
+        self.url_anpr_get_picture = 'http://' + URL + '/ISAPI/ContentMgmt/search'
+
         self.username = username
         self.password = password
+
         self.raw_high_request = '<IOPortData version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema"><outputState>high</outputState></IOPortData>'
         self.raw_low_request = '<IOPortData version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema"><outputState>low</outputState></IOPortData>'
         self.raw_http_hosts_request = """
@@ -31,6 +35,53 @@ class Camera:
                 </ANPR>
             </HttpHostNotification>
         </HttpHostNotificationList>"""
+        self.body_anpr_get_plates = """
+        <AfterTime>
+            <picTime>2020-03-07T21:00:00Z</picTime>
+        </AfterTime>
+        """
+        self.body_anpr_pic_search = """
+        <?xml version: '1.0' encoding='utf-8'?>
+        <CMSearchDescription version="2.0" xmlns="http://www.isapi.org/ver20/XMLSchema">
+            <searchID>C77384AD-66A0-0001-E7C2-1151F04F90B0</searchID>
+            <trackIDList>
+                <trackID>103</trackID>
+            </trackIDList>
+            <timeSpanList>
+                <timeSpan>
+                    <startTime>2020-06-22T02:25:13Z</startTime>
+                    <endTime>2022-06-22T02:25:13Z</endTime>
+                </timeSpan>
+            </timeSpanList>
+            <searchTypeList>
+            </searchTypeList>
+            <contentTypeList>
+                <contentType>metadata</contentType>
+            </contentTypeList>
+            <maxResults>100</maxResults>
+            <searchResultPostion>0</searchResultPostion>
+            <metadataList>
+                <metadataDescriptor>
+                //recordType.meta.std-cgi.com
+                </metadataDescriptor>
+                <SearchProperity>
+                </SearchProperity>
+            </metadataList>
+        </CMSearchDescription>
+        """
+        self.xxy = """
+        <?xml version: '1.0' encoding='utf-8'?>
+        <CMSearchDescription>
+            <searchID>202112181158361310</searchID><trackIDList><trackID>103</trackID></trackIDList>
+            <timeSpanList><timeSpan><startTime>2020-06-22T02:25:13Z</startTime><endTime>2022-06-22T02:25:13Z</endTime></timeSpan></timeSpanList>
+            <contentTypeList><contentType>metadata</contentType></contentTypeList>
+            <maxResults>100</maxResults><searchResultPostion>0</searchResultPostion>
+            <metadataList>
+                <metadataDescriptor>//recordType.meta.std-cgi.com/vehicleDetection</metadataDescriptor>
+                <SearchProperity><plateSearchMask/><nation>255</nation></SearchProperity>
+            </metadataList>
+        </CMSearchDescription>
+        """
 
     def postOutputRequest(self, state) -> None:
         if state is 1:
@@ -53,6 +104,11 @@ class Camera:
         return response
 
     def getAnprPlates(self):
-        response = requests.get(self.url_anpr_get_plates, auth=HTTPDigestAuth(self.username, self.password))
+        response = requests.post(self.xxx, data=self.body_anpr_get_plates, auth=HTTPDigestAuth(self.username, self.password))
+
+        return response
+
+    def getAnprPicture(self):
+        response = requests.post(self.url_anpr_get_picture, data=self.body_anpr_pic_search, auth=HTTPDigestAuth(self.username, self.password))
 
         return response
